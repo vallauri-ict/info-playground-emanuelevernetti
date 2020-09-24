0da9c4b39bf3c097cbe3d6104bbb37418bd9481d
Emanuele Vernetti <58587228+emanuelevernetti@users.noreply.github.com>
2020-09-24 12:24:10 +0200

    commit

---
+++ FormulaOneSolution/FormulaOneConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FormulaOneConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
