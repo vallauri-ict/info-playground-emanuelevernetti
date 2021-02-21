e595fb5a677084101acf697141aaf83c96a7de94
Emanuele Vernetti <e.vernetti.1033@vallauri.edu>
2021-02-21 11:03:02 +0100

    Es stored procedure

---
+++ 02-TriggerTestAssieme/TriggerTestAssieme/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace TriggerTestAssieme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDriver = new System.Windows.Forms.DataGridView();
+            this.dgvStoricoCancellazioni = new System.Windows.Forms.DataGridView();
+            this.dgvStoricoAggiornamenti = new System.Windows.Forms.DataGridView();
+            this.btnAggDB = new System.Windows.Forms.Button();
+            this.btnSP = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDriver)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStoricoCancellazioni)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStoricoAggiornamenti)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDriver
+            // 
+            this.dgvDriver.AllowUserToAddRows = false;
+            this.dgvDriver.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDriver.Location = new System.Drawing.Point(13, 13);
+            this.dgvDriver.Name = "dgvDriver";
+            this.dgvDriver.Size = new System.Drawing.Size(323, 196);
+            this.dgvDriver.TabIndex = 0;
+            this.dgvDriver.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.OnDblClickDriver);
+            this.dgvDriver.RowsRemoved += new System.Windows.Forms.DataGridViewRowsRemovedEventHandler(this.OnRowsDeletedDriver);
+            // 
+            // dgvStoricoCancellazioni
+            // 
+            this.dgvStoricoCancellazioni.AllowUserToAddRows = false;
+            this.dgvStoricoCancellazioni.AllowUserToDeleteRows = false;
+            this.dgvStoricoCancellazioni.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStoricoCancellazioni.Location = new System.Drawing.Point(352, 13);
+            this.dgvStoricoCancellazioni.Name = "dgvStoricoCancellazioni";
+            this.dgvStoricoCancellazioni.RowHeadersVisible = false;
+            this.dgvStoricoCancellazioni.Size = new System.Drawing.Size(323, 196);
+            this.dgvStoricoCancellazioni.TabIndex = 1;
+            // 
+            // dgvStoricoAggiornamenti
+            // 
+            this.dgvStoricoAggiornamenti.AllowUserToAddRows = false;
+            this.dgvStoricoAggiornamenti.AllowUserToDeleteRows = false;
+            this.dgvStoricoAggiornamenti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStoricoAggiornamenti.Location = new System.Drawing.Point(13, 242);
+            this.dgvStoricoAggiornamenti.Name = "dgvStoricoAggiornamenti";
+            this.dgvStoricoAggiornamenti.RowHeadersVisible = false;
+            this.dgvStoricoAggiornamenti.Size = new System.Drawing.Size(662, 196);
+            this.dgvStoricoAggiornamenti.TabIndex = 2;
+            // 
+            // btnAggDB
+            // 
+            this.btnAggDB.Location = new System.Drawing.Point(13, 216);
+            this.btnAggDB.Name = "btnAggDB";
+            this.btnAggDB.Size = new System.Drawing.Size(159, 23);
+            this.btnAggDB.TabIndex = 3;
+            this.btnAggDB.Text = "Aggiorna Data Base";
+            this.btnAggDB.UseVisualStyleBackColor = true;
+            this.btnAggDB.Click += new System.EventHandler(this.btnAggDB_Click);
+            // 
+            // btnSP
+            // 
+            this.btnSP.Location = new System.Drawing.Point(178, 216);
+            this.btnSP.Name = "btnSP";
+            this.btnSP.Size = new System.Drawing.Size(111, 23);
+            this.btnSP.TabIndex = 4;
+            this.btnSP.Text = "S.P.";
+            this.btnSP.UseVisualStyleBackColor = true;
+            this.btnSP.Click += new System.EventHandler(this.btnSP_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 450);
+            this.Controls.Add(this.btnSP);
+            this.Controls.Add(this.btnAggDB);
+            this.Controls.Add(this.dgvStoricoAggiornamenti);
+            this.Controls.Add(this.dgvStoricoCancellazioni);
+            this.Controls.Add(this.dgvDriver);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDriver)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStoricoCancellazioni)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStoricoAggiornamenti)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDriver;
+        private System.Windows.Forms.DataGridView dgvStoricoCancellazioni;
+        private System.Windows.Forms.DataGridView dgvStoricoAggiornamenti;
+        private System.Windows.Forms.Button btnAggDB;
+        private System.Windows.Forms.Button btnSP;
+    }
+}
+
---
+++ 02-TriggerTestAssieme/TriggerTestAssieme/Form1.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.VisualBasic;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TriggerTestAssieme
+{
+    public partial class Form1 : Form
+    {
+        public static readonly string workingDirectory = Environment.CurrentDirectory;
+        public static readonly string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+        public static readonly string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + projectDirectory + @"\DriverDB.mdf;Integrated Security=True;Connect Timeout=30";
+
+        private BindingSource bsDriver = new BindingSource();
+        private BindingSource bsStoricoCancellazioni = new BindingSource();
+        private BindingSource bsStoricoAggiornamenti = new BindingSource();
+        private DataTable dtDriver, dtStoricoCancellazioni, dtStoricoAggiornamenti;
+        private SqlDataAdapter daDriver, daStoricoCancellazioni, daStoricoAggiornamenti;
+
+        private void OnDblClickDriver(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void OnRowsDeletedDriver(object sender, DataGridViewRowsRemovedEventArgs e)
+        {
+            daDriver.Update(dtDriver);
+            //MessageBox.Show("Riga cancellata su DB");
+            PopulateDgv(2);
+        }
+
+        private void btnAggDB_Click(object sender, EventArgs e)
+        {
+            daDriver.Update(dtDriver);
+            PopulateDgv(0);
+        }
+
+        private void btnSP_Click(object sender, EventArgs e)
+        {
+            DateTime dt = Convert.ToDateTime(Interaction.InputBox("Inserisci data"));
+            using (SqlConnection con = new SqlConnection(CONNECTION_STRING))
+            {
+                con.Open();
+                string sql = "restituisciDati";
+                SqlCommand cmd = new SqlCommand(sql, con);
+
+                SqlParameter data = new SqlParameter();
+                data.ParameterName = "data";
+                data.Direction = ParameterDirection.Input;
+                data.DbType = DbType.DateTime;
+                data.Value = dt;
+                cmd.Parameters.Add(data);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable dataTable = new DataTable();
+                dataTable.Load(reader);
+                dgvDriver.DataSource = reader;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Query(string selectCommand, out SqlDataAdapter da)
+        {
+            da = null;
+            try
+            {
+                // Crea un nuovo data adapter basato su selectCommand
+                da = new SqlDataAdapter(selectCommand, CONNECTION_STRING);
+
+                // Creo un command builder per generare un comando SQL update, insert, delete
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(da);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopulateDgv(0);
+        }
+
+        private void PopulateDgv(int tables)
+        {
+            switch (tables)
+            {
+                case 0:
+                    dgvDriver.DataSource = bsDriver;
+                    Query("SELECT * FROM Driver", out daDriver);
+                    if (daDriver != null)
+                    {
+                        dtDriver = new DataTable();
+                        daDriver.Fill(dtDriver);
+                        bsDriver.DataSource = dtDriver;
+                    }
+                    dgvDriver.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
+                    
+                    dgvStoricoCancellazioni.DataSource = bsStoricoCancellazioni;
+                    Query("SELECT * FROM StoricoCancellazioni", out daStoricoCancellazioni);
+                    if (daStoricoCancellazioni != null)
+                    {
+                        dtStoricoCancellazioni = new DataTable();
+                        daStoricoCancellazioni.Fill(dtStoricoCancellazioni);
+                        bsStoricoCancellazioni.DataSource = dtStoricoCancellazioni;
+                    }
+
+                    dgvStoricoCancellazioni.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
+                    
+                    dgvStoricoAggiornamenti.DataSource = bsStoricoAggiornamenti;
+                    Query("SELECT * FROM StoricoAggiornamenti", out daStoricoAggiornamenti);
+                    if (daStoricoAggiornamenti != null)
+                    {
+                        dtStoricoAggiornamenti = new DataTable();
+                        daStoricoAggiornamenti.Fill(dtStoricoAggiornamenti);
+                        bsStoricoAggiornamenti.DataSource = dtStoricoAggiornamenti;
+                    }
+
+                    dgvDriver.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
+                    break;
+                case 1:
+                    dgvDriver.DataSource = bsDriver;
+                    Query("SELECT * FROM Driver", out daDriver);
+                    if (daDriver != null)
+                    {
+                        dtDriver = new DataTable();
+                        daDriver.Fill(dtDriver);
+                        bsDriver.DataSource = dtDriver;
+                    }
+                    dgvDriver.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
+                    break;
+                case 2:
+                    dgvStoricoCancellazioni.DataSource = bsStoricoCancellazioni;
+                    Query("SELECT * FROM StoricoCancellazioni", out daStoricoCancellazioni);
+                    if (daStoricoCancellazioni != null)
+                    {
+                        dtStoricoCancellazioni = new DataTable();
+                        daStoricoCancellazioni.Fill(dtStoricoCancellazioni);
+                        bsStoricoCancellazioni.DataSource = dtStoricoCancellazioni;
+                    }
+
+                    dgvStoricoCancellazioni.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
+                    break;
+                case 3:
+                    dgvStoricoAggiornamenti.DataSource = bsStoricoAggiornamenti;
+                    Query("SELECT * FROM StoricoAggiornamenti", out daStoricoAggiornamenti);
+                    if (daStoricoAggiornamenti != null)
+                    {
+                        dtStoricoAggiornamenti = new DataTable();
+                        daStoricoAggiornamenti.Fill(dtStoricoAggiornamenti);
+                        bsStoricoAggiornamenti.DataSource = dtStoricoAggiornamenti;
+                    }
+
+                    dgvDriver.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
+                    break;
+            }
+        }
+    }
+}
