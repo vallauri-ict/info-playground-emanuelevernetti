eb375a438f68e2497510455891ef3bd0f420fba8
Emanuele <e.vernetti.1033@vallauri.edu>
2021-03-04 10:08:26 +0100

    Esercizio statistiche piloti

---
+++ StatistichePiloti/StatistichePiloti/FormMain.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace StatistichePiloti
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcedure = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnProcedure
+            // 
+            this.btnProcedure.Location = new System.Drawing.Point(131, 11);
+            this.btnProcedure.Name = "btnProcedure";
+            this.btnProcedure.Size = new System.Drawing.Size(75, 23);
+            this.btnProcedure.TabIndex = 0;
+            this.btnProcedure.Text = "Procedure";
+            this.btnProcedure.UseVisualStyleBackColor = true;
+            this.btnProcedure.Click += new System.EventHandler(this.btnProcedure_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(13, 14);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 329);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.btnProcedure);
+            this.Name = "Form1";
+            this.Text = "Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcedure;
+        private System.Windows.Forms.TextBox txtID;
+    }
+}
+
---
+++ StatistichePiloti/StatistichePiloti/FormMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StatistichePiloti
+{
+    public partial class FormMain : Form
+    {
+        private static string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFileName=" + Directory.GetCurrentDirectory() + @"\FormulaOne.mdf;Integrated Security=True";
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcedure_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtID.Text);
+
+            using (SqlConnection con = new SqlConnection(CONNECTION_STRING))
+            {
+                con.Open();
+                string sql = "generaStatistiche";
+                SqlCommand cmd = new SqlCommand(sql, con);
+
+                SqlParameter driverId = new SqlParameter();
+                driverId.ParameterName = "driverID";
+                driverId.Direction = ParameterDirection.Input;
+                driverId.DbType = DbType.Int32; //Tipo di dato nella S.P.
+                driverId.Value = id;
+                cmd.Parameters.Add(driverId);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
